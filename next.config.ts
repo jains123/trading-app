0ea85f6183fb5de1dbd96dbb9ddf5202244a1366
Jamie Ainsworth <jamie.ainsworth@nous.partners>
2026-03-24 22:54:03 +0000

    main

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
-const nextConfig: NextConfig = {
-  serverExternalPackages: ['yahoo-finance2'],
-  outputFileTracingRoot: path.join(__dirname, '../'),
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
